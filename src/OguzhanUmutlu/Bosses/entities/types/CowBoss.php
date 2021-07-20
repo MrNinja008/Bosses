daf03365dba05ebbbaf44a30bdd8cf72476fd013
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2021-07-20 18:47:40 +0300

    fix for cow

--- src/OguzhanUmutlu/Bosses/entities/types/CowBoss.php
+++ src/OguzhanUmutlu/Bosses/entities/types/CowBoss.php
@@ -4,7 +4,7 @@ namespace OguzhanUmutlu\Bosses\entities\types;
 
 use OguzhanUmutlu\Bosses\entities\BossEntity;
 
-class P extends BossEntity {
+class CowBoss extends BossEntity {
     public const NETWORK_ID = self::COW;
     public function getName(): string {
         return "CowBoss";
