51b778238fe41ca94d20b8a36b75ac9bae01256f
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2021-07-20 23:06:15 +0300

    improved events with drops

--- src/OguzhanUmutlu/Bosses/events/BossDeathEvent.php
+++ src/OguzhanUmutlu/Bosses/events/BossDeathEvent.php
@@ -7,12 +7,24 @@ use pocketmine\event\Event;
 
 class BossDeathEvent extends Event {
     private $boss;
-    public function __construct(BossEntity $boss) {
+    private $drops;
+    public function __construct(BossEntity $boss, array $drops) {
         $this->boss = $boss;
+        $this->drops = $drops;
     }
 
     /*** @return BossEntity */
     public function getBoss(): BossEntity {
         return $this->boss;
+    }
+
+    /*** @return array */
+    public function getDrops(): array {
+        return $this->drops;
+    }
+
+    /*** @param array $drops */
+    public function setDrops(array $drops): void {
+        $this->drops = $drops;
     }
 }
--- src/OguzhanUmutlu/Bosses/events/MinionDeathEvent.php
+++ src/OguzhanUmutlu/Bosses/events/MinionDeathEvent.php
@@ -7,12 +7,24 @@ use pocketmine\event\Event;
 
 class MinionDeathEvent extends Event {
     private $minion;
-    public function __construct(BossEntity $minion) {
+    private $drops;
+    public function __construct(BossEntity $minion, array $drops) {
         $this->minion = $minion;
+        $this->drops = $drops;
     }
 
     /*** @return BossEntity */
     public function getMinion(): BossEntity {
         return $this->minion;
+    }
+
+    /*** @return array */
+    public function getDrops(): array {
+        return $this->drops;
+    }
+
+    /*** @param array $drops */
+    public function setDrops(array $drops): void {
+        $this->drops = $drops;
     }
 }
